4f20f97f3fb0f3982016ec1441eccebc2bd2575e
Ryan Jay Deculawan <68984500+RJ0514@users.noreply.github.com>
2021-07-19 09:12:29 +0800

    Add files via upload
    Will organize this one further.

---
+++ Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    private int nRank;
+    private boolean bAlive;
+	
+	public int getRank()
+	{
+		return nRank;
+	}
+	
+	public boolean getLive()
+	{
+		return bAlive;
+	}
+	
+	
+	public void setRank(int rank)	//Always include the return type in declaration.
+	{
+		nRank = rank;
+	}
+}
---
+++ Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    int nNumber;
+    long lColor;
+    boolean bWin;
+
+    public boolean checkWin(boolean win){ // this is stupid HAHAHAHA
+        return win;
+    }
+}
