0b8b19d1416713d3bb034a582f908b958d32e18a
Samuel Uy <samuel_uy@dlsu.edu.ph>
2021-08-28 15:30:22 +0800

    wow GUI HAHAHA

--- src/GUI.java
+++ src/GUI.java
@@ -4,30 +4,27 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI implements ActionListener {
+public class GUI{
 
     public GUI() {
         GameBoard board = new GameBoard();
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        JLabel label = new JLabel("picture/tiger.png");
-        JButton button = new JButton("Restart");
-        button.addActionListener(this);
 
-
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
+        JPanel toolbar = new JPanel();
+        JButton restart = new JButton("Restart");
+        JButton exit = new JButton("Exit");
+        JButton showInstructions = new JButton("Instructions");
 
         frame.add(panel,BorderLayout.CENTER);
-        frame.add(label);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Animal Chess");
-        frame.pack();
+        frame.setSize(400,600);
         frame.setVisible(true);
-    }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println("Nice");
+        toolbar.setLayout(new FlowLayout());
+        toolbar.add(showInstructions);
+        toolbar.add(restart);
+        toolbar.add(exit);
+        frame.add(toolbar, BorderLayout.NORTH);
     }
 }
